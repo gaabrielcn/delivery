33c354db9276b4099dab31c19cca5e687acba7e9
gaabrielcn <bibicn14@gmail.com>
2025-05-12 08:39:15 -0300

    Reestruturando diretorio

--- src/main/java/com/delivery/deliverymds/repository/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.delivery.deliverymds.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.delivery.deliverymds.model.Cliente;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-}
